433331a160ad94472af796b8667866d0712695eb
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-08-16 21:04:32 +0200

    step 1

--- options.js
+++ options.js
@@ -1,185 +1,195 @@
+$(document).ready(function() {
 
-function deleteRow(rowTr) {
-	var mainTableBody = document.getElementById('mainTableBody');
-	mainTableBody.removeChild(rowTr);
-}
+	/*
+	const dateFormat = "yy-mm-dd";
 
-function isNumeric(str) {
-  if (typeof str != "string") {return false;} // we only process strings!
-  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-         !isNaN(parseInt(str)) // ...and ensure strings of whitespace fail
-}
+	$.fn.dataTable.ext.search.push(
+		function( settings, data, dataIndex ) {
 
-function createTableRow(feed) {
-	var mainTableBody = document.getElementById('mainTableBody');
-	var tr = mainTableBody.insertRow();
+			const min = (function() {
+				const from_str = $('#from').val();
+				if(from_str !== "" ) {
+					try {
+						const date = $.datepicker.parseDate( dateFormat, from_str);
+						return parseInt( date.getTime(), 10 );
+					}catch(e){
+						console.error(e);
+					}
+				}
+				return NaN;
+			})();
 
-	Object.keys(feed).sort().forEach( (key) => {
+			const max = (function() {
+				const to_str = $('#to').val();
+				if(to_str !== "" ) {
+					try {
+						const date = $.datepicker.parseDate( dateFormat, to_str);
+						return parseInt( date.getTime(), 10 );
+					}catch(e){
+						console.error(e);
+					}
+				}
+				return NaN;
+			})();
 
-		var input = document.createElement('input');
-		input.className = key;
-		input.style.width = '95%';
-		switch (key) {
-			case 'activ':
-				input.placeholder = key;
-				input.type='checkbox';
-				input.checked= (typeof feed[key] === 'boolean') ? feed[key]: true;
-				break;
-			case 'annotation':
-				input.placeholder = key;
-				input.value = feed[key];
-				break;
-			case 'code':
-				input.placeholder = 'css_selector';
-				input.value = feed[key];
-				break;
-			case 'delay':
-				input.placeholder = '0';
-				input.value = (feed[key] > -1) ? feed[key] : 0;
-				input.type='number';
-				input.min=0;
-				break;
-			case 'repeat':
-				input.placeholder = '0';
-				input.value = (feed[key] > -1) ? feed[key] : 0;
-				input.type='number';
-				input.min=0;
-				break;
-			case 'url_regex':
-				input.placeholder = 'url_regex';
-				input.value = feed[key];
-				break;
-			default:
-				return;
-		}
-		tr.insertCell().appendChild(input);
-	});
+			const age = parseFloat( data[0] ) || 0; // use data for the age column
 
-	var button;
-	if(feed.action === 'save'){
-		button = createButton("Create", "saveButton", function() {}, true );
-	}else{
-		button = createButton("Delete", "deleteButton", function() { deleteRow(tr); }, false );
-	}
-	tr.insertCell().appendChild(button);
-}
+			console.log('min: ' + min, " max: ", max, " value: ", data[0]);
 
-function collectConfig() {
-	var mainTableBody = document.getElementById('mainTableBody');
-	var feeds = [];
-	for (var row = 0; row < mainTableBody.rows.length; row++) { 
-		try {
-			var url_regex = mainTableBody.rows[row].querySelector('.url_regex').value.trim() || '';
-			var ses = mainTableBody.rows[row].querySelector('.code').value || '';
-			var desc = mainTableBody.rows[row].querySelector('.annotation').value.trim() || '';
-			var check = mainTableBody.rows[row].querySelector('.activ').checked || false ;
-			var repeat = mainTableBody.rows[row].querySelector('.repeat').value || 0 ;
-			var delay = mainTableBody.rows[row].querySelector('.delay').value || 0;
+			return ( ( isNaN( min ) && isNaN( max ) ) ||
+				( isNaN( min ) && age <= max ) ||
+				( min <= age   && isNaN( max ) ) ||
+				( min <= age   && age <= max ) );
+		});
+	*/
 
-			if(url_regex !== '' && ses !== '' && isNumeric(delay) && isNumeric(repeat)) {
-				delay = parseInt(delay);
-				repeat = parseInt(repeat);
-				feeds.push({
-					'activ': check,
-					'annotation': desc, 
-					'url_regex': url_regex,
-					'code': ses,
-					'delay': ( typeof delay !== 'number' || delay < 0)? 0 : delay,
-					'repeat': repeat
-				});
+	console.log('init mainTable');
+
+	let table = $('#mainTable').DataTable( {
+		'columnDefs': [
+			{ orderable: false , targets: 2}
+		],
+		'processing': true,
+		'language': {
+			//'processing': '<div id="outerProgress"><div id="innerProgress"></div></div>',
+			'loadingRecords': "Loading,<br/>please wait"
+		},
+		"deferRender": true,
+		"stateSave": true,
+		"ajax": async function(data, callback, settings)  {
+
+			//var res = await browser.storage.local.get('selectors');
+			//
+			const rows = [
+				{
+					// activ, desc, selector, delay, repeat, url-regex, actions
+					 'activ': true
+					,'desciption': ''
+					,'selector': ''
+					,'delay': 1000
+					,'repeat': 0
+					,'regex': ''
+					
+				}
+
+			];
+
+			callback({data: rows});
+		},
+		"dom": '<"top"flip>rt<"bottom"flip>',
+		"lengthMenu": [ [25, 50, 100, 250, 500, -1], [25, 50, 100, 250, 500, "All"] ],
+		"columns": [
+			{ "data": "activ"
+				, "render": function(data, type, row, meta) {
+					return type === 'display' ? '<input style="width:98%;" type="checkbox" '+(data?'checked':'')+' />' : data;
+				}
 			}
-		}catch(e){
-			console.error(e);
-		}
-	}
-	return feeds;
-}
-
-function createButton(text, id, callback, submit) {
-	var span = document.createElement('span');
-	var button = document.createElement('button');
-	button.id = id;
-	button.textContent = text;
-	button.className = "browser-style";
-	if (submit) {
-		button.type = "submit";
-	} else {
-		button.type = "button";
-	}
-	button.name = id;
-	button.value = id;
-	button.addEventListener("click", callback);
-	span.appendChild(button);
-	return span;
-}
-
-async function saveOptions(e) {
-	var feeds = collectConfig();
-	await browser.storage.local.set({ 'selectors': feeds });
-}
-
-async function restoreOptions() {
-	var mainTableBody = document.getElementById('mainTableBody');
-	createTableRow({
-		'activ': 1,
-		'annotation': '',
-		'code': '' ,
-		'url_regex': '',
-		'action':'save',
-		'delay' : -1,
-		'repeat': -1,
+			,{ "data": "desciption"
+				, "render": function(data, type, row, meta) {
+					return type === 'display' ? '<input style="width:98%;" type="text" value="' + data + '" />' : data;
+				}
+			}
+			,{ "data": "selector"
+				, "render": function(data, type, row, meta) {
+					return type === 'display' ? '<input style="width:98%;" type="text" value="' + data + '" />' : data;
+				}
+			}
+			,{ "data": "delay"
+				,"render": function(data, type,row, meta) {
+					return type === 'display' ? '<input type="number" value="' +data+ '" />' : data;
+				}
+			}
+			,{ "data": "repeat"
+				,"render": function(data, type,row, meta) {
+					return type === 'display' ? '<input type="number" value="' +data+ '" />' : data;
+				}
+			}
+			,{ "data": "regex"
+				, "render": function(data, type, row, meta) {
+					return type === 'display' ? '<input style="width:98%;" type="text" value="' + data + '" />' : data;
+				}
+			}
+		]
 	});
-	var res = await browser.storage.local.get('selectors');
-	if ( !Array.isArray(res.selectors) ) { return; }
-	res.selectors.forEach( (selector) => {
-		selector.action = 'delete'
-		if(typeof selector.annotation !== 'string') {
-			selector.annotation = '';
-		}
 
-		if(typeof selector.repeat !== 'number') {
-			selector.repeat = -1;
-		}
-		createTableRow(selector);
+	$('#myTable tbody').on( 'click', 'tr', function () {
+		$(this).toggleClass('selected');
 	});
-}
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+	/*
+	$('#removeDisplayed').click(function() {
+		const filteredRows = table.rows({page: 'current', filter: 'applied'});
+		const filteredRows_data = filteredRows.data();
+		const filteredRows_len = filteredRows_data.length;
+		if(filteredRows_len < 1){
+			alert('nothing to delete on the current page');
+			return;
+		}
+		if (!confirm('Are you sure you want delete all displayed (aka. currently visible) entries?')) {
+			return;
+		}
+		filteredRows.remove().draw();
+		for(let i = 0;i < filteredRows_len;++i) {
+			idbKeyval.del(filteredRows_data[i].url);
+		}
+	});
 
-const impbtnWrp = document.getElementById('impbtn_wrapper');
-const impbtn = document.getElementById('impbtn');
-const expbtn = document.getElementById('expbtn');
+	$('#removeSelected').click( function () {
+		const filteredRows = table.rows('.selected'); 
+		const filteredRows_data = filteredRows.data();
+		const filteredRows_len = filteredRows_data.length;
+		if(filteredRows_len < 1){
+			alert('no rows selected');
+			return;
+		}
+		if (!confirm('Are you sure you want delete the selected entries?')) {
+			return;
+		}
+		filteredRows.remove().draw();
+		for(let i = 0;i < filteredRows_len;++i) {
+			idbKeyval.del(filteredRows_data[i].url);
+		}
+	});
 
-expbtn.addEventListener('click', async function (evt) {
-    var dl = document.createElement('a');
-    var res = await browser.storage.local.get('selectors');
-    var content = JSON.stringify(res.selectors,null,4);
-    dl.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(content));
-    dl.setAttribute('download', 'data.json');
-    dl.setAttribute('visibility', 'hidden');
-    dl.setAttribute('display', 'none');
-    document.body.appendChild(dl);
-    dl.click();
-    document.body.removeChild(dl);
-});
+	let from = $( "#from" )
+		.datepicker({
+			dateFormat: dateFormat,
+			//defaultDate: "+1w",
+			changeMonth: true,
+			changeYear: true,
+			numberOfMonths: 1
+		})
+		.on( "change", function() {
+			to.datepicker( "option", "minDate", getDate( this ) );
+			table.draw();
+		});
 
-// delegate to real Import Button which is a file selector
-impbtnWrp.addEventListener('click', function(evt) {
-	impbtn.click();
-})
+	let to = $( "#to" ).datepicker({
+		dateFormat: dateFormat,
+		//defaultDate: "+1w",
+		changeMonth: true,
+		changeYear: true,
+		numberOfMonths: 1
+		})
+		.on( "change", function() {
+			from.datepicker( "option", "maxDate", getDate( this ) );
+			table.draw();
+		});
 
-impbtn.addEventListener('input', function (evt) {
-	var file  = this.files[0];
-	var reader = new FileReader();
-	        reader.onload = async function(e) {
-            try {
-                var config = JSON.parse(reader.result);
-		await browser.storage.local.set({ 'selectors': config});
-		document.querySelector("form").submit();
-            } catch (e) {
-                console.error('error loading file: ' + e);
-            }
-        };
-        reader.readAsText(file);
-});
+	$('#resetFrom').click(function(){ $('#from').val(''); table.draw();});
+	$('#resetTo').click(function(){ $('#to').val('');table.draw(); });
+
+	function getDate( element ) {
+			console.log('getDate',element.value);
+		try {
+			return $.datepicker.parseDate( dateFormat, element.value );
+		} catch( error ) {
+			console.error(error);
+		}
+		return null;
+	}
+
+	*/
+
+} );
+
