dd4ee08b34e802285eff9c73c7fb015233d54985
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-02-12 20:10:32 +0100

    feat(#17) add a way to toggle Enabled state of grouped rules feat(#15) make rules table remember the manual sorting order

--- options.js
+++ options.js
@@ -4,41 +4,76 @@ const impbtnWrp = document.getElementById('impbtn_wrapper');
 const impbtn = document.getElementById('impbtn');
 const savbtn= document.getElementById('savbtn');
 const expbtn = document.getElementById('expbtn');
+const delbtn = document.getElementById('delbtn');
+const ablebtn = document.getElementById('ablebtn');
+const addbtn = document.getElementById('addbtn');
 
+addbtn.addEventListener('click', async function (evt) {
+
+                table.addRow({
+                    enabled: false,
+                    group: '',
+                    annotation: '',
+                    tags: '',
+                    cssselector: '',
+                    initaldelay: 0,
+                    repeatdelay: 0,
+                    randomrepeatvariance: 0,
+                    urlregex: '',
+                    action: 'Add'
+                },true);
+
+        savbtn.style.background='red';
+});
+
+ablebtn.addEventListener('click', async function (evt) {
+    let changed = false;
+    const rows = table.getSelectedRows();
+    for(const row of rows){
+        const cell = row.getCell('enabled');
+        if(cell.setValue(!cell.getValue())){
+            changed = true;
+        }
+    }
+    if(changed){
+        savbtn.style.background='red';
+    }
+});
+
+delbtn.addEventListener('click', async function (evt) {
+    let changed = false;
+    const rows = table.getSelectedRows();
+    for(const row of rows){
+        row.delete();
+        changed = true;
+    }
+    if(changed){
+        savbtn.style.background='red';
+    }
+
+});
 savbtn.addEventListener('click', async function (evt) {
     let data = table.getData();
     let i=0;
-
     for(i=0; i<data.length;i++){
-        if(data[i].action === 'Add'){
-            data.splice(i,1);
-            break;
-        }
-    }
-
-    for(i=0; i<data.length;i++){
+        // numbers need parsing ... for whatever reason
         data[i].initaldelay = parseInt(data[i].initaldelay);
         data[i].repeatdelay = parseInt(data[i].repeatdelay);
         data[i].randomrepeatvariance= parseInt(data[i].randomrepeatvariance);
+        data[i].pos = i;
     }
-    // remove Add Row
     browser.storage.local.set({ 'selectors': data })
     savbtn.style.background='lightgreen';
 });
 
 expbtn.addEventListener('click', async function (evt) {
     var dl = document.createElement('a');
-    let data = table.getData();
-    for(let i=0; i<data.length;i++){
-        if(data[i].action === 'Add'){
-            data.splice(i,1);
-            break;
-        }
-    }
+    var data = table.getSelectedData();
     for(i=0; i<data.length;i++){
         data[i].initaldelay = parseInt(data[i].initaldelay);
         data[i].repeatdelay = parseInt(data[i].repeatdelay);
         data[i].randomrepeatvariance= parseInt(data[i].randomrepeatvariance);
+        data[i].pos = i;
     }
     const content = JSON.stringify(data,null,4);
     dl.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(content));
@@ -73,8 +108,7 @@ impbtn.addEventListener('input', function (evt) {
                         initaldelay: parseInt(selector.delay || selector.initaldelay || 0),
                         repeatdelay: parseInt(selector.repeat || selector.repeatdelay || 0),
                         randomrepeatvariance: parseInt(selector.rvariance || selector.randomrepeatvariance || 0),
-                        urlregex: selector.url_regex || selector.urlregex || '',
-                        action: 'Delete'
+                        urlregex: selector.url_regex || selector.urlregex || ''
                     }, false);
                     imported_something = true;
                 });
@@ -88,75 +122,20 @@ impbtn.addEventListener('input', function (evt) {
         reader.readAsText(file);
 });
 
-
-// formatter func for the action column
-function btnFormatter(cell, formatterParams, onRendered){
-    return '<button style="width:100%" >' + cell.getValue() + '</button>';
-}
-
-// onClick handler for the action cells
-function actionHandler(e,cell) {
-    switch( cell.getValue() ){
-        case 'Add':
-            cell.setValue("Delete",true);
-            table.addRow({
-                enabled: true,
-                annotation: '',
-                group: '#Default Group',
-                tags: '',
-                cssselector: '',
-                initaldelay: 0,
-                repeatdelay: 0,
-                randomrepeatvariance: 0,
-                urlregex: '',
-                action: 'Add'
-            },true);
-            break;
-        default:
-            const rows = table.searchRows("action", "=","Add");
-            rows[0].delete();
-            cell.getRow().delete().then(function() {
-                browser.storage.local.set({ 'selectors': table.getData() })
-                table.addRow({
-                    enabled: true,
-                    group: '#Default Group',
-                    annotation: '',
-                    tags: '',
-                    cssselector: '',
-                    initaldelay: 0,
-                    repeatdelay: 0,
-                    randomrepeatvariance: 0,
-                    urlregex: '',
-                    action: 'Add'
-                },true);
-                savbtn.style.background='red';
-            });
-            //table.redraw();
-            break;
-    }
-}
-
-function tagFilter(headerValue, rowValue, rowData, filterParams){
-    console.log(headerValue, rowValue, rowData, filterParams);
-    //headerValue - the value of the header filter element
-    //rowValue - the value of the column in this row
-    //rowData - the data for the row being filtered
-    //filterParams - params object passed to the headerFilterFuncParams property
-    //
-    if(typeof rowValue !== 'string'){
-        return true;
-    }
-    const tags = rowValue.split(", ");
-
-    for(const tag of tags) {
-        if(tag.includes(headerValue)){
-            return true;
+function tagValuesLookup (){
+    const rows = table.getRows();
+    const tags = [];
+    for(const row of rows){
+        const cell = row.getCell('tags');
+        const vals = cell.getValue().split(/[\s,]+/);
+        for(const val of vals){
+            if(val !== ''){
+                tags.push(val);
+            }
         }
     }
-    return false;
+    return tags;
 }
-
-
 
 async function onDOMContentLoaded() {
 
@@ -176,7 +155,7 @@ async function onDOMContentLoaded() {
         movableRows: true,
         initialSort:[             //set the initial sort order of the data
             {column:"group", dir:"asc"},
-            {column:"action", dir:"asc"},
+            //{column:"action", dir:"asc"},
         ],
         groupBy: "group",
         groupUpdateOnCellEdit:true,
@@ -191,31 +170,44 @@ async function onDOMContentLoaded() {
         groupStartOpen: true,
         layoutColumnsOnNewData:true,
         columns:[
-            //{rowHandle:true, formatter:"handle", headerSort:false, frozen:true, width:30, minWidth:30},
-            /*{formatter:"rowSelection", titleFormatter:"rowSelection", width:30, minWidth:30, hozAlign:"left", headerSort:false, cellClick:function(e, cell){
+            {rowHandle:true, formatter:"handle", headerSort:false, frozen:true, width:30, minWidth:30, },
+            {formatter:"rowSelection", titleFormatter:"rowSelection", width:30, minWidth:30, hozAlign:"left", headerSort:false, cellClick:function(e, cell){
                 cell.getRow().toggleSelect();
-            }},*/
+            }},
             {title:"Enabled", width: 100, field:"enabled", formatter: "tickCross", sorter:"boolean", headerHozAlign: "center",  hozAlign:"center", editor:true, editorParams: { tristate:false}},
-            {title:"Group", field:"group", headerFilter:"input", headerFilterPlaceholder:"Filter by Group", editor:"input", sorter: "string", sorterParams: {locale: true, alignEmptyValues: "top"}},
-            {title:"Annotation", field:"annotation", headerFilter:"input", headerFilterPlaceholder:"Filter by Annotation", editor:"input", sorter: "string", sorterParams: {locale: true, alignEmptyValues: "top"}},
-            {title:"Tags", field:"tags", headerFilter:"input", headerFilterPlaceholder:"Filter by Tag/Label", editor:"input", sorter: "string", sorterParams: {locale: true, alignEmptyValues: "bottom"}, headerFilterFunc:tagFilter },
-            {title:"CSS Selector", field:"cssselector", headerFilter:"input", headerFilterPlaceholder:"Filter by CSS Selector",editor:"input"},
+            {title:"Group", field:"group", headerFilter:"input", headerFilterPlaceholder:"Textfilter", editor:"input", sorter: "string", sorterParams: {locale: true, alignEmptyValues: "top"}},
+            {title:"Annotation", field:"annotation", headerFilter:"input", headerFilterPlaceholder:"Textfilter", editor:"input", sorter: "string", sorterParams: {locale: true, alignEmptyValues: "top"}},
+            {title:"Tags", field:"tags", headerFilter:"select", headerFilterPlaceholder:"Multiselect", editor:"input", sorter: "string", sorterParams: {locale: true, alignEmptyValues: "bottom"} , headerFilterParams:{
+                values: tagValuesLookup, // get values
+                verticalNavigation:"hybrid", //navigate to new row when at the top or bottom of the selection lis
+                multiselect:true, //allow multiple entries to be selected
+
+            }
+
+
+            },
+            {title:"CSS Selector", field:"cssselector", headerFilter:"input", headerFilterPlaceholder:"Textfilter",editor:"input"},
             {title:"Inital <br/>Delay", width: 100, field:"initaldelay", sorter:"number", editor:"input", headerSort: false, validator: ['required','min:0', 'integer'] },
             {title:"Repeat <br/>Delay", width: 100, field:"repeatdelay", sorter:"number", editor:"input", headerSort: false, validator: ['required','min:0', 'integer']},
             {title:'<acronym title="Random Repeat Variance" style="text-decoration-style:dashed;">RRV</acronym>', headerSort: false, width: 120, field:"randomrepeatvariance", sorter:"number", editor:"input", validator: ['required','min:0', 'integer']},
-            {title:'URL <acronym title="Regular Expression" style="text-decoration-style:dashed;">RegEx</acronym>', field:"urlregex",headerFilter:"input", headerFilterPlaceholder:"Filter by URL RegEx",editor:"input"},
-            {title:"Action", width: 100, field:"action", hozAlign:"center", headerHozAlign: "center",  formatter: btnFormatter, headerSort: false, cellClick: actionHandler}
+            {title:'URL <acronym title="Regular Expression" style="text-decoration-style:dashed;">RegEx</acronym>', field:"urlregex",headerFilter:"input", headerFilterPlaceholder:"Textfilter",editor:"input"}
+            //{title:"Single Action", width: 100, field:"action", hozAlign:"center", headerHozAlign: "center",  formatter: btnFormatter, headerSort: false, cellClick: actionHandler}
         ],
     });
 
     table.on("cellEdited", function(cell){
-        if( cell.getRow().getData()['action'] !== 'Add') {
+        //if( cell.getRow().getData()['action'] !== 'Add') {
             const new_val = cell.getValue();
             const old_val = cell.getOldValue();
             if(new_val != old_val){
                 savbtn.style.background='red';
             }
-        }
+        //}
+    });
+
+    table.on("rowMoved", function(row){
+        //row - row component
+        savbtn.style.background='red';
     });
 
     /*
@@ -236,6 +228,7 @@ async function onDOMContentLoaded() {
     });
 
     // add input row
+    /*
     table.addRow({
             enabled: true,
             group: '#Default Group',
@@ -248,6 +241,7 @@ async function onDOMContentLoaded() {
             urlregex: '',
             action: 'Add'
         },true);
+        */
 
 
 
@@ -267,12 +261,78 @@ async function onDOMContentLoaded() {
         //rows[0].move(1, true);
     });
     */
+
+    table.on("groupClick", function(e, group){
+        //e - the click event object
+        //group - group component
+        console.log('groupClick');
+
+        const rows = group.getRows();
+        for(const row of rows){
+
+            //const cell = row.getCell("enabled");
+            //cell.setValue(!cell.getValue())
+            row.toggleSelect();
+
+        }
+    });
+
+    /*
+    table.on("dataSorted", function(_sorters, _rows){
+        //sorters - array of the sorters currently applied
+        //rows - array of row components in their new order
+            const rows = table.searchRows("action", "=","Add");
+            rows[0].delete();
+            cell.getRow().delete().then(function() {
+                browser.storage.local.set({ 'selectors': table.getData() })
+                table.addRow({
+                    enabled: true,
+                    group: '#Default Group',
+                    annotation: '',
+                    tags: '',
+                    cssselector: '',
+                    initaldelay: 0,
+                    repeatdelay: 0,
+                    randomrepeatvariance: 0,
+                    urlregex: '',
+                    action: 'Add'
+                },true);
+                savbtn.style.background='red';
+            });
+
+    });
+    */
 }
 
 async function getTblData() {
     let data = [];
     var res = await browser.storage.local.get('selectors');
+
     if ( Array.isArray(res.selectors) ) {
+        res.selectors.sort(function(b,a){
+            // > 0 => b before a
+            // < 0 => a before b
+            // === 0 => keep original order of a and b
+
+            if(typeof a.pos === 'undefined' && typeof b.pos === 'number'){
+                return 1; // b before a
+            }
+            if(typeof a.pos === 'number' && typeof b.pos === 'undefined'){
+                return -1; // a before b
+            }
+
+            if(typeof a.pos === 'number' && typeof b.pos === 'number'){
+                if(a.pos > b.pos){
+                    return 1;
+                }
+                if(a.pos < b.pos){
+                    return -1;
+                }
+            }
+            // if in doubt, do nothing :) , also covers  double undeinfed and a === b
+            return 0;
+
+        });
         res.selectors.forEach( (selector) => {
             data.push({
                 enabled: selector.activ || selector.enabled || false,
@@ -283,8 +343,8 @@ async function getTblData() {
                 initaldelay: selector.delay || selector.initaldelay || 1000,
                 repeatdelay: selector.repeat || selector.repeatdelay || 0,
                 randomrepeatvariance: parseInt(selector.rvariance || selector.randomrepeatvariance || 0),
-                urlregex: selector.url_regex || selector.urlregex || '',
-                action: 'Delete'
+                urlregex: selector.url_regex || selector.urlregex || ''
+                //action: 'Delete'
             });
         });
     }
